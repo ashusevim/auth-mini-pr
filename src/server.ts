c99668342e5695e82cf286b51b65d62647abfbd9
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-11-11 22:08:52 +0530

    Implemented refresh token

--- src/server.ts
+++ src/server.ts
@@ -1,130 +1,156 @@
-import express, { type Request, type Response } from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import bcypt from "bcrypt"
-import { users, type User } from "./users.js"
-import jwt from "jsonwebtoken"
-import { authenticate } from "./middleware/auth.js"
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import bcypt from "bcrypt";
+import { users, type User } from "./users.js";
+import jwt from "jsonwebtoken";
+import { authenticate } from "./middleware/auth.js";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signup', async (req: Request, res: Response) => {
-    try {
-        const { name, email, password } = req.body
+app.post("/signup", async (req: Request, res: Response) => {
+	try {
+		const { name, email, password, refreshToken } = req.body;
 
-        // validate
-        if (!name || !email || !password) {
-            return res.status(400).json({ message: "All the fields are required" })
-        }
+		// validate
+		if (!name || !email || !password) {
+			return res
+				.status(400)
+				.json({ message: "All the fields are required" });
+		}
 
-        // look for an existing user
-        const existingUser = users.find((user) => { user.email == email })
+		// look for an existing user
+		const existingUser = users.find((user) => {
+			user.email == email;
+		});
 
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" })
-        }
+		if (existingUser) {
+			return res.status(400).json({ message: "User already exists" });
+		}
 
-        // saltrounds shows how many times the hash is internally processed
-        const saltRound = 10;
-        const hashedPassword = await bcypt.hash(password, saltRound)
+		// saltrounds shows how many times the hash is internally processed
+		const saltRound = 10;
+		const hashedPassword = await bcypt.hash(password, saltRound);
 
-        // create an object for a new user
-        const newUser: User = {
-            id: users.length + 1,
-            name,
-            email,
-            hashedPassword,
-        }
+		// create an object for a new user
+		const newUser: User = {
+			id: users.length + 1,
+			name,
+			email,
+			hashedPassword,
+			refreshToken
+		};
 
-        // push the new user to the in-memory demo database
-        users.push(newUser)
-        return res.status(201).json({ message: "User registered successfully", userId: newUser.id })
-    } catch (error) {
-        return res.status(500).json({ message: "Error while registering user!!" })
-    }
-})
+		// push the new user to the in-memory demo database
+		users.push(newUser);
+		return res.status(201).json({
+			message: "User registered successfully",
+			userId: newUser.id,
+		});
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: "Error while registering user!!" });
+	}
+});
 
-app.post('/login', async (req: Request, res: Response) => {
-    const { email, password } = req.body
+app.post("/login", async (req: Request, res: Response) => {
+	const { email, password } = req.body;
 
-    // validate input
-    if (!email || !password) {
-        return res.status(400).json({
-            message: "Email and password are required"
-        })
-    }
+	// validate input
+	if (!email || !password) {
+		return res.status(400).json({
+			message: "Email and password are required",
+		});
+	}
 
-    // find user
-    const user = users.find((user) => user.email === email);
-    if (!user) {
-        return res.status(404).json({
-            message: "User not found!!"
-        })
-    }
+	// find user
+	const user = users.find((user) => user.email === email);
+	if (!user) {
+		return res.status(404).json({
+			message: "User not found!!",
+		});
+	}
 
-    // it basically checks whether the password entered by the user mactches with the stored password
-    const compare_password = await bcypt.compare(password, user.hashedPassword)
-    if (!compare_password) {
-        return res.status(401).json({
-            message: "Invalid credentials"
-        })
-    }
+	// it basically checks whether the password entered by the user mactches with the stored password
+	const compare_password = await bcypt.compare(password, user.hashedPassword);
+	if (!compare_password) {
+		return res.status(401).json({
+			message: "Invalid credentials",
+		});
+	}
 
-    const jwt_secret = process.env.JWT_SECRET;
-    if (!jwt_secret) {
-        return res.status(401).json({
-            message: "JWT_SECRET is not provided"
-        })
-    }
+	const jwt_secret = process.env.JWT_SECRET;
+	if (!jwt_secret) {
+		return res.status(401).json({
+			message: "JWT_SECRET is not provided",
+		});
+	}
 
-    // generate JWT
-    const token = jwt.sign({ id: user.id, email: user.email }, jwt_secret, { expiresIn: "15m" })
+	// Generate access token (JWT)
+	const token = jwt.sign({ id: user.id, email: user.email }, jwt_secret, {
+		expiresIn: "15m",
+	});
 
-    //send response
-    return res.status(200).json({
-        message: "Login successful",
-        token: token
-    })
-})
+	// Generate a refresh token (long lived)
+	const refreshToken = jwt.sign(
+		{ id: user.id, email: user.email },
+		jwt_secret,
+		{
+			expiresIn: "7d",
+		},
+	);
 
-app.get('/users', (req: Request, res: Response) => {
-    return res.status(200).json({ users })
-})
+	user.refreshToken = refreshToken;
 
-app.get('/users/:id', (req: Request, res: Response) => {
-    try {
-        const { id } = req.body;
-        if (!id) {
-            return res.status(400).json({ message: "All the fields are required" })
-        }
+	//send response
+	return res.status(200).json({
+		message: "Login successful",
+		token, // short lived
+		refreshToken, // long lived
+	});
+});
 
-        const user = users.find((user) => user.id == id)
-        return res.status(200).json({ user })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Failed to find the user with the given ID"
-        })
-    }
-})
+app.get("/users", (req: Request, res: Response) => {
+	return res.status(200).json({ users });
+});
 
-app.get('/dashboard', authenticate, (req: Request, res: Response) => {
-    // req.user is available and typed (refer to the types.d.ts)
-    const user = req.user
-    return res.status(200).json({
-        message: `Welcome to the dashboard,  ${user?.email}`
-    })
-})
+app.get("/users/:id", (req: Request, res: Response) => {
+	try {
+		const { id } = req.body;
+		if (!id) {
+			return res
+				.status(400)
+				.json({ message: "All the fields are required" });
+		}
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Auth is running...')
-})
+		const user = users.find((user) => user.id == id);
+		return res.status(200).json({ user });
+	} catch (error) {
+		return res.status(500).json({
+			message: "Failed to find the user with the given ID",
+		});
+	}
+});
+
+app.get("/dashboard", authenticate, (req: Request, res: Response) => {
+	// req.user is available and typed (refer to the types.d.ts)
+	const user = req.user;
+	return res.status(200).json({
+		message: `Welcome to the dashboard,  ${user?.email}`,
+	});
+});
+
+app.get("/", (req: Request, res: Response) => {
+	res.send("Auth is running...");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on ${port}`)
-})
+	console.log(`Server running on ${port}`);
+});
--- src/users.ts
+++ src/users.ts
@@ -1,9 +1,10 @@
-export interface User{
-    id: number;
-    name: string;
-    email: string;
-    hashedPassword: string;
+export interface User {
+	id: number;
+	name: string;
+	email: string;
+	hashedPassword: string;
+	refreshToken: string;
 }
 
 // it will hold the "registered users"
-export const users: User[] = []
+export const users: User[] = [];
