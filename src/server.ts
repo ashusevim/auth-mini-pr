dfa9dea2a5cf6913a2540aff3ba00fca41f51b6a
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-11-12 13:06:46 +0530

    FIXED some basic bugs

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import bcypt from "bcrypt";
 import { users, type User } from "./users.js";
 import jwt from "jsonwebtoken";
 import { authenticate } from "./middleware/auth.js";
-import { emit } from "node:process";
 
 dotenv.config();
 
@@ -28,7 +27,7 @@ app.post("/signup", async (req: Request, res: Response) => {
 		}
 
 		// look for an existing user
-		const existingUser = users.find(user => user.email === email);
+		const existingUser = users.find((user) => user.email === email);
 
 		if (existingUser) {
 			return res.status(400).json({ message: "User already exists" });
@@ -61,62 +60,71 @@ app.post("/signup", async (req: Request, res: Response) => {
 });
 
 app.post("/login", async (req: Request, res: Response) => {
-	const { email, password } = req.body;
+	try {
+		const { email, password } = req.body;
 
-	// validate input
-	if (!email || !password) {
-		return res.status(400).json({
-			message: "Email and password are required",
+		// validate input
+		if (!(email || password)) {
+			return res.status(400).json({
+				message: "Email and password are required",
+			});
+		}
+
+		// find user
+		const user = users.find((user) => user.email === email);
+		if (!user) {
+			return res.status(404).json({
+				message: "User not found!!",
+			});
+		}
+
+		// it basically checks whether the password entered by the user mactches with the stored password
+		const compare_password = await bcypt.compare(
+			password,
+			user.hashedPassword,
+		);
+		if (!compare_password) {
+			return res.status(401).json({
+				message: "Invalid credentials",
+			});
+		}
+
+		// Generate access token (JWT)
+		const token = jwt.sign(
+			{ id: user.id, email: user.email },
+			jwt_secret as string,
+			{
+				expiresIn: "15m",
+			},
+		);
+
+		// Generate a refresh token (long lived)
+		const refreshToken = jwt.sign(
+			{ id: user.id, email: user.email },
+			jwt_secret as string,
+			{
+				expiresIn: "7d",
+			},
+		);
+
+		user.refreshToken = refreshToken;
+
+		//send response
+		return res.status(200).json({
+			message: "Login successful",
+			token, // short lived
+			refreshToken, // long lived
+		});
+	} catch (error) {
+		return res.status(500).json({
+			message: "Error while login user"
 		});
 	}
-
-	// find user
-	const user = users.find(user => user.email === email);
-	if (!user) {
-		return res.status(404).json({
-			message: "User not found!!",
-		});
-	}
-
-	// it basically checks whether the password entered by the user mactches with the stored password
-	const compare_password = await bcypt.compare(password, user.hashedPassword);
-	if (!compare_password) {
-		return res.status(401).json({
-			message: "Invalid credentials",
-		});
-	}
-
-	// Generate access token (JWT)
-	const token = jwt.sign(
-		{ id: user.id, email: user.email },
-		jwt_secret as string,
-		{
-			expiresIn: "15m",
-		},
-	);
-
-	// Generate a refresh token (long lived)
-	const refreshToken = jwt.sign(
-		{ id: user.id, email: user.email },
-		jwt_secret as string,
-		{
-			expiresIn: "7d",
-		},
-	);
-
-	user.refreshToken = refreshToken;
-
-	//send response
-	return res.status(200).json({
-		message: "Login successful",
-		token, // short lived
-		refreshToken, // long lived
-	});
 });
 
 app.get("/logout", (req: Request, res: Response) => {
 	const { refreshToken } = req.body;
-	const user = users.find(user => user.refreshToken === refreshToken);
+	const user = users.find((user) => user.refreshToken === refreshToken);
 	if (user) user.refreshToken = "";
 	return res.status(200).json({
 		message: "Logged out successfully",
@@ -133,7 +141,7 @@ app.get("/refresh", async (req: Request, res: Response) => {
 	}
 
 	// find the user who owns the refreshToken
-	const user = users.find(user => user.refreshToken === refreshToken);
+	const user = users.find((user) => user.refreshToken === refreshToken);
 	if (!user) {
 		return res.status(403).json({
 			message: "Invalid refresh token",
@@ -175,7 +183,7 @@ app.get("/users/:id", (req: Request, res: Response) => {
 				.json({ message: "All the fields are required" });
 		}
 
-		const user = users.find(user => user.id == id);
+		const user = users.find(user => user.id === id);
 		return res.status(200).json({ user });
 	} catch (error) {
 		return res.status(500).json({
@@ -184,7 +192,7 @@ app.get("/users/:id", (req: Request, res: Response) => {
 	}
 });
 
-app.get("/dashboard", authenticate, (req: Request, res: Response) => {
+app.post("/dashboard", authenticate, (req: Request, res: Response) => {
 	// req.user is available and typed (refer to the types.d.ts)
 	const user = req.user;
 	return res.status(200).json({
